8528353d1273e3a25052405008e95bc0d0fe5f0e
tatsuya-kato08 <78143445+tatsuya-kato08@users.noreply.github.com>
2021-02-16 19:18:25 +0900

    実装課題修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -28,6 +28,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
   end
 
   def update
